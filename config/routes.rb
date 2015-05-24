640771e5c1b3c99087c1ee646264e459f9726db3
Miguel Alfredo Garcia Baca <imiked.mail@gmail.com>
2015-05-23 23:56:33 -0500

    Descargar curriculum terminado

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
    root 'home#index'
-
+   get 'resume' => 'resume#pdf'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
